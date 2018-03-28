02bba164e87c81d2a8a4a4038356487ed99fb588
Azlan Mustapha <ajmustapha@gmail.com>
2018-03-27 21:07:58 -0700

    Added cascade delete functionality and fixed naming convention

--- db/CreateCartTable.sql
+++ db/CreateCartTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE Cart(
   cart_id SERIAL,
 item_id INTEGER NOT NULL,
 item_quantity INTEGER NOT NULL,
-customer_id INTEGER NOT NULL,
+customer_id INTEGER NOT NULL REFERENCES Customers(customer_id) ON DELETE CASCADE,
 PRIMARY KEY (cart_id),
 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 FOREIGN KEY (item_id) REFERENCES Items(item_id));
--- db/CreateCategoryTable.sql
+++ db/CreateCategoryTable.sql
@@ -1,20 +1,20 @@
 DROP TABLE IF EXISTS Category;
 CREATE TABLE Category(
   category_id SERIAL,
-name VARCHAR(50) NOT NULL,
+category_name VARCHAR(50) NOT NULL,
 PRIMARY KEY (category_id));
 
-INSERT INTO Category (name)
+INSERT INTO Category (category_name)
 VALUES ('Clothes');
 
-INSERT INTO Category (name)
+INSERT INTO Category (category_name)
 VALUES ('Hats');
 
-INSERT INTO Category (name)
+INSERT INTO Category (category_name)
 VALUES ('Toiletries');
 
-INSERT INTO Category (name)
+INSERT INTO Category (category_name)
 VALUES ('Household');
 
-INSERT INTO Category (name)
+INSERT INTO Category (category_name)
 VALUES ('Electronics');
