e55d172b470e96254c426bd4dfdccd86bd56516d
Azlan Mustapha <ajmustapha@gmail.com>
2018-03-29 15:43:53 -0700

    Fixed createall.sql, added config to sequelize.js

--- db/CreateAll.sql
+++ db/CreateAll.sql
@@ -183,43 +183,55 @@ CREATE TABLE Loyalty(
   FOREIGN KEY (item_id) REFERENCES Items(item_id),
   CHECK (cart_quantity >= 0));
 
+
   INSERT INTO Cart (item_id, cart_quantity, customer_id)
   VALUES (1, 5, 1);
   INSERT INTO Cart (item_id, cart_quantity, customer_id)
   VALUES (2, 3, 1);
   INSERT INTO Cart (item_id, cart_quantity, customer_id)
   VALUES (3, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 1, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 2, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 3, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 4, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 5, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 6, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 7, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 8, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 9, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 10, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 11, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 12, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 13, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 14, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 15, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 16, 1, 2);
-  INSERT INTO Cart (cart_id, item_id, item_quantity, customer_id)
+  INSERT INTO Cart (cart_id, item_id, cart_quantity, customer_id)
   VALUES (3, 17, 1, 2);
+
+  DROP VIEW IF EXISTS CategoryAvgPrice;
+  CREATE VIEW CategoryAvgPrice (category_id, category_name, avg) AS
+  SELECT C.category_id, C.category_name, AVG(price) AS avg_price
+  FROM Items I NATURAL JOIN Category C
+  GROUP BY C.category_id;
+
+  DROP VIEW IF EXISTS CartCustomersItems;
+  CREATE VIEW CartCustomersItems (cart_id, customer_id, name, item_id, item_name, category_id, category_name, cart_quantity, item_quantity) AS
+  SELECT Cart.cart_id AS "Cart ID", Cart.customer_id AS "Customer ID", Customers.name AS "Name", Cart.item_id AS "Item ID", Items.item_name AS "Item", category_id AS "Category ID", category_name AS "Category", Cart.cart_quantity AS "In Cart", Items.item_quantity AS "Stock"
+  FROM Cart NATURAL JOIN Customers NATURAL JOIN Items NATURAL JOIN Category;
--- server/configs/sequelize.js
+++ server/configs/sequelize.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "Demo",
-  process.env.DB_USER || "postgres",
-  process.env.DB_PASSWORD || "postgres",
+  process.env.DB_NAME || "qirawywi",
+  process.env.DB_USER || "qirawywi",
+  process.env.DB_PASSWORD || "jV9wLDvzBeQt5vIP-DMODjEscAbkgbiR",
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "baasu.db.elephantsql.com",
     port: parseInt(process.env.DB_PORT) || 5432,
     dialect: "postgres",
     pool: {
